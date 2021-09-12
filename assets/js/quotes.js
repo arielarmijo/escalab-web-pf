67445504329c9ab0f48f4b7a758ea8e8b48a839f
Ariel Armijo <arielarmijo@yahoo.es>
2021-09-12 13:08:12 -0300

    Agrega citas al azar

---
+++ assets/js/quotes.js
@@ -0,0 +1,20 @@
+
+const quotes = './assets/js/quotes.json';
+const blockquote = document.getElementById('quote');
+
+const getRandomQuote = () => fetch(quotes)
+                                .then(response => response.json())
+                                .then(data => data[Math.floor(Math.random()*data.length)]);
+
+setInterval(() => {
+    getRandomQuote().then(quote => {
+        blockquote.classList.remove('fade');
+        setTimeout(() => {
+            blockquote.innerHTML = `<p class="quote-body">${quote.quote}</p>
+                                <p class="quote-footer">${quote.author}</p>`;
+            blockquote.classList.add('fade');
+        }, 255);
+    });
+}, 1000);
+
+
